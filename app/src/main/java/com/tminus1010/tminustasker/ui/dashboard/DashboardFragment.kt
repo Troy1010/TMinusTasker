91e1fdb1342ffed1d8b282fc79ac0a0f3e655ec0
Troy <troyswanson1010@gmail.com>
2024-03-14 15:53:33 -0500

    DashboardFragment`view`layout is nicer

--- app/src/main/java/com/tminus1010/tminustasker/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/tminus1010/tminustasker/ui/dashboard/DashboardFragment.kt
@@ -5,10 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.ComposeView
+import androidx.compose.ui.unit.dp
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.tminus1010.tminustasker.databinding.FragmentDashboardBinding
@@ -43,10 +49,24 @@ class DashboardFragment : Fragment() {
 
 @Composable
 fun StringList(strings: List<String>) {
-    Column {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp)
+    ) {
         strings.forEach { s ->
-            Card {
-                Text(text = s)
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 8.dp),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color.Green
+                ),
+            ) {
+                Text(
+                    text = s,
+                    modifier = Modifier.padding(16.dp)
+                )
             }
         }
     }
