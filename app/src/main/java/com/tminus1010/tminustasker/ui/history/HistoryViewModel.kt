1a8d497c762fb853613c5ae7bf6c0481bdcf33ad
Troy <troyswanson1010@gmail.com>
2024-03-30 02:01:49 -0500

    HistoryViewModel considers date adjustment, but is less reactive

--- app/src/main/java/com/tminus1010/tminustasker/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/tminus1010/tminustasker/ui/history/HistoryViewModel.kt
@@ -2,29 +2,26 @@ package com.tminus1010.tminustasker.ui.history
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tminus1010.tmcommonkotlin.tuple.tuple
-import com.tminus1010.tminustasker.data.CurrentDateRepo
 import com.tminus1010.tminustasker.domain.LocalDateTimeToLocalDateWithAdjustmentConverter
 import com.tminus1010.tminustasker.domain.MainInteractor
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import java.time.LocalDateTime
 import javax.inject.Inject
 
 @HiltViewModel
 class HistoryViewModel @Inject constructor(
     private val mainInteractor: MainInteractor,
-    private val currentDateRepo: CurrentDateRepo,
     private val localDateTimeToLocalDateWithAdjustmentConverter: LocalDateTimeToLocalDateWithAdjustmentConverter,
 ) : ViewModel() {
     val state =
-        combine(mainInteractor.taskCompletions, currentDateRepo.flow, ::tuple)
-            .map { (taskCompletions, currentDate) ->
+        mainInteractor.taskCompletions
+            .map { taskCompletions ->
                 taskCompletions
                     .groupBy { localDateTimeToLocalDateWithAdjustmentConverter.convert(it.localDateTime) }
-                    .mapKeys { (key, value) -> "$key${if (key == currentDate) " (Today)" else ""}" }
+                    .mapKeys { (key, value) -> "$key${if (key == localDateTimeToLocalDateWithAdjustmentConverter.convert(LocalDateTime.now())) " (Today)" else ""}" } // TODO: Make Reactive
             }
             .stateIn(viewModelScope, SharingStarted.Eagerly, mapOf())
 }
